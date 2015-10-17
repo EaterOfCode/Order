8649a2410d47f49adad0d622f303bc15719b9a6e
Corne Oppelaar <hello@eaterofco.de>
2015-10-17 23:14:32 +0200

    don't use services in void docker

--- storage/nginx.law.php
+++ storage/nginx.law.php
@@ -5,16 +5,19 @@ $package = 'nginx';
 package($package)
     ->install();
 
-service($package)
-    ->enable()
-    ->requires(package($package));
+
+if (!in_array(paper('os.distrobution'), ['void'])) {
+    service($package)
+        ->enable()
+        ->requires(package($package));
+}
 
 $wwwFolder = '/var/local/www/';
 $nginxConfig = which(paper('os.distrobution'), ["freebsd" => '/usr/local/etc/nginx/nginx.conf', '/etc/nginx/nginx.conf']);
 
 file($nginxConfig)
     ->contents(file_get_contents(__DIR__ . '/nginx/nginx.conf'))
-    ->requires(service($package))
+    ->requires(package($package))
     ->notify(service($package));
 
 dir($wwwFolder)
